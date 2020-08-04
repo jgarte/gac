70c8e6ec72d2b802e97fc5df00d5e39e57bfbb7d
Christian Jaeger <ch@christianjaeger.ch>
2020-08-04 02:23:36 +0100

    correct now, even though we don't know why at this time

--- lily.scm
+++ lily.scm
@@ -23,37 +23,77 @@
 (define (keyword->string k)
   (symbol->string (keyword->symbol k)))
 
+(define (=symbol? a)
+  (let* ((s (symbol->string a))
+         (l (string-length s)))
+    (and (>= l 2)
+         (eq? (string-ref s 0) #\=))))
+
+(define (=symbol->string a)
+  (let* ((s (symbol->string a))
+         (l (string-length s)))
+    (and (>= l 2)
+         (substring s 1 l))))
+
+
+(define (display-chord l port)
+  (diplayl port
+           "<"
+           (map note->string l)
+           ">"))
+
 (define (lily->file l path)
   (call-with-output-file path
     (lambda (port)
       (let lf ((l l)
 	       (is-header? #f))
 	(xcond ((pair? l)
-		(let ((a (car l)))
-		 (xcond ((keyword? a)
-			 (displayl port "\\" (keyword->string a)) 
-			 (for-each (lambda (l)
-				     (display " " port)
-				     (lf l #t))   
-				   (cdr l))
-			 (newline port)))))
+		(let ((a (car l))
+                      (process-rest
+                       (lambda ()
+                         (let* ((v (xone (cdr l))))
+                           (cond ((pair? v)
+                                  (display " = {\n" port)
+                                  (for-each (lambda (l)
+                                              (display " " port)
+                                              (lf l #t))
+                                            v)
+                                  (display "}" port))
+                                 (else
+                                  (display " " port)
+                                  (lf v #t))))
+                         (newline port))))
+                  (cond ((_keyword? a)
+                         (displayl port "\\" (keyword->string a)) 
+                         (process-rest))
+                        ((symbol? a)
+                         (if is-header?
+                             (begin
+                               (assert (=symbol? a))
+                               (displayl port (=symbol->string a) " =")
+                               (process-rest))
+                             (display-chord l port)))
+                        (else
+                         ;; treat l as an actual list, not as an "AST" node
+                         (for-each (lambda (v)
+                                     (lf v is-header?))
+                                   l)))))
 	       ((string? l)
 		(write l port)))))))
 
 (define h
-`(
- (#:version "2.17.2")
- (#:glanguage "english")
- (#:header ((=tagline "foo")
-	    (=author "bar")))
- (c4 e4 g4)))
+  `((#:version "2.17.2")
+    (#:glanguage "english")
+    (#:header ((=tagline "foo")
+               (=author "bar")))
+    (c4 e4 g4)))
 
 (define (t1)
   (lily->file '(#:version "2.3.4") "t1"))
 
 (define (t2)
-  (lily->file '(#:header (=tagline "foo") (=author "bar")) "t2"))
+  (lily->file '(#:header ((=tagline "foo") (=author "bar"))) "t2"))
 
 (define (t)
-  (lily->file t "foo.ly"))
+  (lily->file h "foo.ly"))
 
---
+++ note.scm
@@ -0,0 +1,148 @@
+;; representation 1: integers
+
+
+;; representation 2: scientificnote (symbols)  c4
+;; https://en.wikipedia.org/wiki/Scientific_pitch_notation
+
+'(define (integer->scientificnote i)
+  )
+
+(define (scientificnote-parse-base str)
+  (if (string-empty? str)
+      0
+      (let ((n (string->number str)))
+        (if (and n
+                 (integer? n)
+                 (>= n 0))
+            n
+            (error "invalid scientific note base:" str)))))
+
+
+(define scientificnote-subs
+  '(C CS D DS E F FS G GS A AS B))
+
+(define lilynote-subs-vector
+  (list->vector (map (lambda (sym)
+                       (string-lc (symbol->string sym)))
+                     scientificnote-subs)))
+
+(define scientificnote-sub-alis
+  (map cons
+       scientificnote-subs
+       (iota 12)))
+
+(define (scientificnote-parse-sub ch)
+  (cond ((assq (string->symbol ch) scientificnote-sub-alis)
+         => cdr)
+        (else
+         (error "invalid scientificnote sub:" ch))))
+
+(define (scientificnote->integer sym)
+  (let* ((s (symbol->string sym))
+         (len (string-length s)))
+    (if (>= len 1)
+        (let lp ((i 0))
+          (if (< i len)
+              (let ((c (string-ref s i)))
+                (if (char-digit? c)
+                    (+ (scientificnote-parse-sub (substring s 0 i))
+                       (* 12
+                          (scientificnote-parse-base (substring s i len)))
+                       -48)
+                    (lp (inc i))))
+              (+ (scientificnote-parse-sub (substring s 0 i)) (* 12 4) -48)
+              ;; or:
+              ;; (error "scientificnote without octave not accepted for not"
+              ;;        sym)
+              ))
+        (error "invalid scientificnote:" sym))))
+
+
+(TEST
+ > (scientificnote->integer 'C3)
+ -12
+ > (scientificnote->integer 'C4)
+ 0
+ > (scientificnote->integer 'C5)
+ 12
+ > (scientificnote->integer 'C3)
+ -12
+ > (scientificnote->integer 'CS3)
+ -11)
+
+
+;; representation 3: lily note string
+
+(define (integer->quotient+remainder i d)
+  (values (quotient i d)
+          (remainder i d)))
+
+(define (quotient+remainer->integer q r d)
+  (+ (* q d) r))
+
+(define (flub i d)
+  "the whole part of the dividion of i by d, but in a way that it fits modulo--?"
+  (if (negative? i)
+      (dec (quotient i d))
+      (quotient i d)))
+
+(TEST
+ > (flub 10 12)
+ 0
+ > (flub 11 12)
+ 0
+ > (flub 12 12)
+ 1
+ > (flub 0 12)
+ 0
+ > (flub -1 12)
+ -1
+ > (flub -10 12)
+ -1
+ > (flub -11 12)
+ -1
+ > (flub -12 12)
+ -2)
+
+
+(define (integer->flub+modulo i d)
+  (values (flub i d)
+          (modulo i d)))
+
+(define (flub+modulo->integer q r d)
+  (+ (* q d) r))
+
+
+(define (lilynote-base-format i)
+  (cond ((= i 4) "")
+        ((< i 4) (make-string (- 4 i) #\,))
+        (else (make-string (- i 4) #\'))))
+
+
+(define (integer->lilynote i)
+  (string-append
+   (vector-ref lilynote-subs-vector (modulo i 12))
+   (let ((f (flub i 12)))
+     (if (or (= i -1) (= i -12)) ;; ridiculous
+         ""
+         (lilynote-base-format (+ 5 f))))))
+
+
+(TEST
+ > (integer->lilynote 1)
+ "cs'"
+ > (integer->lilynote 0)
+ "c'"
+ > (integer->lilynote -1)
+ "b"
+ > (integer->lilynote -2)
+ "as"
+ > (integer->lilynote -12)
+ "c"
+ > (integer->lilynote -13)
+ "b,"
+ )
+
+;; (define (lilynote->integer str)
+;;   )
+
