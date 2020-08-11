07b7595323e5f659125ed0a28aa3306a6f84c212
Christian Jaeger <ch@christianjaeger.ch>
2020-08-11 21:55:28 +0100

    example: useful functionality for a start

--- example.scm
+++ example.scm
@@ -2,12 +2,31 @@
          sg1
          lily)
 
-(define (score-display chords #!optional (port (current-output-port)))
-  (lilyscore-display
-   `((#:version "2.18.2")
-     (#:glanguage "english")
-     (#:header ((=tagline "")
-                (=author "")))
-     (lexps ,@chords))
-   port))
+(export chords->lilyscore
+        chords->file
+        lily)
+
+(define (chords->lilyscore chords)
+  `((#:version "2.18")
+    (#:language "english")
+    (#:header ((=tagline "")
+               (=author "")))
+    (lexps ,@chords)))
+
+;; (define (chords->file chords)
+;;   (lilyscore->file (chords->lilyscore chords)))
+
+(define debug? #t)
+
+(define (chords->file chords file)
+  (let ((sc (chords->lilyscore chords)))
+    (when debug?
+      (lilyscore-display sc (current-output-port)))
+    (lilyscore->file sc file)))
+
+(define (lily chords)
+  (let ((file "chords.ly"))
+    (chords->file chords file)
+    (when (zero? (xsystem "lilypond" file))
+      (xsystem "xpdf" (string-append (basename file ".ly") ".pdf")))))
 
