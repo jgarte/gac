b3b94dd939426059a2dd620627fbf4846463bd61
Christian Jaeger <ch@christianjaeger.ch>
2020-08-12 01:32:57 +0100

    guile: fix

--- guile.scm
+++ guile.scm
@@ -4,6 +4,6 @@
 (define keyword? (either ##keyword? uninterned-symbol?))
 
 (define (keyword->symbol k)
-  (string->symbol (xcond ((##keyword? k) (keyword->string))
+  (string->symbol (xcond ((##keyword? k) (##keyword->string k))
                          ((uninterned-symbol? k) (symbol->string k)))))
 
