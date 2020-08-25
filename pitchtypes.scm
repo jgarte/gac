e4a9131dc979f7b8811951a27261fd65807afd43
Christian Jaeger <ch@christianjaeger.ch>
2020-08-25 21:21:39 +0000

    Create pitchtypes.scm

---
+++ pitchtypes.scm
@@ -0,0 +1,13 @@
+
+(def (pitch-integer? v)
+     (and (exact-integer? v)
+          (<= -99 v 99)))
+
+(def (pitchclass-integer? v)
+     (and (exact-integer? v)
+          (<= 0 v 11)))
+
+(definterface pitchtype
+  (defclass (pitch [pitch-integer? integer]))
+  (defclass (pitchclass [pitchclass-integer? integer])))
+
