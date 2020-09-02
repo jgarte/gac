649e6621c733c98dfe8ebedc357970c4447b3116
Jorge Gomez <jgart@dismail.de>
2020-09-02 00:11:18 +0000

    (broken macro)
    M 2020-09-01 23:47 css.scm

--- css.scm
+++ css.scm
@@ -7,12 +7,14 @@
 
 
 (def (fact n)
+     (warn "fact" n)
      (if (zero? n) 1
          (* n (fact (dec n)))))
 
+'(defmacro (FACT ))
 
 (def f
-     (lambda (m) (+ (fact 10) m)))
+     (lambda (m) (+ (FACT 10) m)))
 
 
 ;; (let ..binds.. ..expr..)
@@ -22,8 +24,25 @@
       `(let ,binds ,expr)
       (raise-source-error _where "invalid keyword")))
 
-(def f*
+'(def f*
      (the (lambda (m) (+ f10 m))
           where:
           (f10 (fact 10))))
 
+'(define-syntax kwote
+  (syntax-rules ()
+    ((kwote exp)
+     (quote exp))))
+'(kwote (foo . bar))
+
+(defmacro (kwote exp)
+  (warn "kwote" exp)
+  `(quote ,exp))
+
+;; (equal? (foo (+ 10 20))
+;;         '((+ 10 20) 30))
+
+(defmacro (foo expr)
+  (def v (quote ,expr))
+  (warn "v=" v)
+  `(list ,v `(list "The result is " ,,expr)))
