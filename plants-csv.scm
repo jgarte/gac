183a49220bc681c361883486d57dd93de85bde5a
Christian Jaeger <ch@christianjaeger.ch>
2020-09-11 23:37:15 +0000

    csv parts squash this later?

--- plants-csv.scm
+++ plants-csv.scm
@@ -1,18 +1,6 @@
 (require easy
          read-csv)
 
-'(def (t)
-     (let-pair ((titles rows)
-                (force (csv-file-stream
-                        "/home/chris/tmp/plant_species_gac_list.csv")))
-               (assert
-                (equal? titles
-                        '("Set-class" "prime form" "Species " "Common name")))
-
-               (.map rows
-                     (lambda (row)
-                       (let-list (()))))))
-
 
 (def (plant_species_gac_list_taxonomies-alist)
      (let-pair ((titles rows)
@@ -28,3 +16,24 @@
                                      (cons (.symbol card)
                                            (.symbol family)))))
                    F)))
+
+
+"
+todo:
+- csv import
+- more aliases: all individual chords (but not the times (transpositions))
+- auto finding of names in chords
+(- maybe time of day functions for corresponding transpositions)
+"
+'(def (t)
+     (let-pair ((titles rows)
+                (force (csv-file-stream
+                        "/home/chris/tmp/plant_species_gac_list.csv")))
+               (assert
+                (equal? titles
+                        '("Set-class" "prime form" "Species " "Common name")))
+
+               (.map rows
+                     (lambda (row)
+                       (let-list (()))))))
+
