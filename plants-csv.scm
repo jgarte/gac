088a51cb2cafe2cecb3e70b4bdb41343eaa45bdf
Jorge Gomez & Christian Jaeger <jgart@dismail.de>
2020-09-18 21:46:45 +0000

    plants: implement species mappings

--- plants-csv.scm
+++ plants-csv.scm
@@ -1,11 +1,11 @@
 (require easy
-         read-csv)
+         read-csv
+         read-csv-util)
 
 
-(def (plant_species_gac_list_taxonomies-alist)
+(def (genus-alist)
      (let-pair ((titles rows)
-                (force (csv-file-stream
-                        "/home/chris/tmp/plant_species_gac_list_taxonomies.csv")))
+                (force (csv-file-stream "genus.csv")))
                (assert
                 (equal? titles
                         '("Chord cardinality " "Plant family")))
@@ -18,6 +18,20 @@
                    F)))
 
 
+
+(def (plain-name->variable-name [string? str])
+     (=> (.list str)
+         (.map (lambda (c)
+                 (case c
+                   ((#\space) #\-)
+                   (else
+                    c))))
+         .string))
+
+(TEST
+ > (plain-name->variable-name "Foo bar")
+ "Foo-bar")
+
 "
 todo:
 - csv import
@@ -25,15 +39,47 @@ todo:
 - auto finding of names in chords
 (- maybe time of day functions for corresponding transpositions)
 "
-'(def (t)
-     (let-pair ((titles rows)
-                (force (csv-file-stream
-                        "/home/chris/tmp/plant_species_gac_list.csv")))
-               (assert
-                (equal? titles
-                        '("Set-class" "prime form" "Species " "Common name")))
+(def (species-alist)
+     (let-pair ((titles rows) (force (csv-file-stream "species.csv")))
 
-               (.map rows
-                     (lambda (row)
-                       (let-list (()))))))
+               (assert (equal? titles
+                               '("Set class" "Species")))
+
+               (=> (.map rows
+                         (lambda (row)
+                           (if (read-csv-util:list-empty? row)
+                               (Nothing)
+                               (Just
+                                (let-list ((setclass species) row)
+                                          (cons (call-with-input-string setclass read-all)
+                                                (plain-name->variable-name
+                                                 (trim-both species))))))))
+                   F
+                   cat-Maybes)))
+
+
+
+'(def (f v)
+     (if (pair? v)
+         ;; instead of: (+ (* (car v) (cdr v)) (* 5 (cdr v)) 10)
+         (let ((x (car v))
+               (y (cdr v)))
+           (+ (* x y) (* 5 y) 10))
+         '()))
+
+(def (f1 a b c)
+      (let (v (read))
+       (let ((x (car v))
+             (y (cdr v)))
+         (+ (* x y) (* 5 y) 10))))
+
+(def (f2 a b c)
+      (let-pair ((x y) (read))
+        (+ (* x y) (* 5 y) 10)))
+
+
+(def (f3 a b c)
+      (let ((x (car (read)))
+            (y (cdr (read))))
+        (+ (* x y) (* 5 y) 10)))
 
--- plants.scm
+++ plants.scm
@@ -1,10 +1,11 @@
 (require easy
          plants-csv
-         sg1)
+         sg1
+         musictypes)
 
-(defmacro (define-plant-aliases)
+(defmacro (define-genus-aliases)
   `(begin
-     ,@(.map (plant_species_gac_list_taxonomies-alist)
+     ,@(.map (genus-alist)
              (lambda-pair ((card family))
                      `(begin
                         (define ,card ,(symbol-append '% card))
@@ -15,5 +16,71 @@
                                      .symbol)
                           ,(symbol-append '% card)))))))
 
-(define-plant-aliases)
+(define-genus-aliases)
+
+
+(defmacro (define-species-aliases)
+  `(begin
+     ,@(.map (species-alist)
+             (lambda-pair ((setclass species-variable-string))
+                          (let ((species-variable (.symbol species-variable-string))
+                                (species-variable-lc
+                                 (.symbol
+                                  (.downcase species-variable-string))))
+                            `(begin
+                               (define ,species-variable ',setclass)
+                               (define ,species-variable-lc ,species-variable)))))))
+
+
+(define-species-aliases)
+
+
+
+(def genus-values-alist
+     (map (lambda (p)
+            (cons (eval (car p)) p))
+          (genus-alist)))
+
+(def species-values-alist
+     (map (lambda (p)
+            (let-pair ((setclass species-variable-name-string) p)
+                      (cons setclass
+                            (.symbol species-variable-name-string))))
+          (species-alist)))
+
+(def (maybe-show-genus [(list-of chord?) chs])
+     (cond ((assoc chs genus-values-alist)
+            => cdr)
+           (else #f)))
+
+(def (maybe-show-species [chord? ch])
+     (cond ((assoc ch species-values-alist)
+            => cdr)
+           (else #f)))
+
+(def chords? (list-of chord?))
+
+(def (genus-chords? v)
+     (and (chords? v)
+          (maybe-show-genus v)
+          #t))
+
+(def (species-chord? v)
+     (and (chord? v)
+          (maybe-show-species v)
+          #t))
+
+
+(def. (genus-chords.show v show)
+  (maybe-show-genus v))
+
+(def. (species-chord.show v show)
+  (maybe-show-species v))
+
+(TEST
+ > (show '(0 0 1))
+ (list 0 0 1)
+ > (show '(0 1 2))
+ Ipomoea-hederifolia)
+
 
