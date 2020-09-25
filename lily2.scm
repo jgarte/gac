ada0e389127bd830920b807707040cc7fcdcf6fd
Jorge Gomez & Christian Jaeger <jgart@dismail.de>
2020-09-25 23:16:38 +0000

    lily2: add tests and todo for lilypond types

--- lily2.scm
+++ lily2.scm
@@ -19,17 +19,46 @@
   (list "#" (object->string v)))
 
 (defclass (lp-setting [keyword? key] [lp-value? value])
-  (defmethod (lilypond-bag _) -> string?
+  (defmethod (lilypond-bag _)
     (list (.lilypond-bag key)
           " = "
-          (.lilypond-bag value))))
+          (.lilypond-bag value)
+          "\n")))
+
+(TEST
+ > (def (t v)
+        (fst (with-output-to-string (lambda () (print (.lilypond-bag v))))))
+ > (t (lp-setting foo: 234))
+ "\\foo = #234\n"
+ > (t (lp-setting foo: "234"))
+ "\\foo = \"234\"\n")
 
 
-
-(defclass (lp-instruction)
+(defclass lp-instruction
   (defclass (lp-version [string? value]))
   (defclass (lp-language [string? value]))
   (defclass (lp-header [(list-of lp-setting?) settings]))
   )
 
 (defclass (lp-document [(list-of lp-instruction?) instructions]))
+
+(TEST
+ > (lp-document (list  
+                 (lp-version "5.24.5")
+                 (lp-language "english")
+                 (lp-header (list (lp-setting tagline: "")
+                                  (lp-setting author: "batman")))))
+ [(lp-document)
+  ([(lp-version) "5.24.5"]
+   [(lp-language) "english"]
+   [(lp-header) ([(lp-setting) tagline: ""]
+                 [(lp-setting) author: "batman"])])])
+
+
+"TODO
+Near:
+Make .lilypond-bag methods for lp-version lp-language, lp-header, and lp-document
+Far:
+create a type for lilypond version statement
+create a type for lilypond language statement"
+
